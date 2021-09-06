d68b8f769d2d44d4906e4f3944fdf3f55b709e60
SantiCamm <santi.cammarano@gmail.com>
2021-09-06 15:27:32 -0300

    bro

--- APL1/ejercicio1.sh
+++ APL1/ejercicio1.sh
@@ -1,28 +1,34 @@
 #!/bin/bash
-validarParametros() {
+#funcA
+help() {
 echo "Error, La sintaxis del script es la siguiente:"
-echo "......: $0 directorio 5"
-} #COMPLETAR
+echo "uso: $0 directorio 5" #COMPLETAR
+} 
+
+#funcB
 error(){
 echo "Error. $1 no es un directorio" #COMPLETAR}
-validarDirectorio(){
+
+#funcC
+validarParametros(){
 if [[ ! -d $2 ]]; then
-funcB
+error
 fi
 }
-funcC $# $1 $2 $3 $4 $5 #validar parametros de entrada
+validarParametros $# $1 $2 $3 $4 $5 #validar parametros de entrada
 LIST=$(ls -d $1*/)
+# LIST = `ls -d $1/` --> otra forma
 # ls -d lista todos las directory entries en el parametro que se le pase (no lista contenidos)
 # */ expande solo a los directorios
 # entonces agregandole -d solo te traes los directorios dentro del parametro pasado
 # EJEMPLO: ls -d * ---> Desktop		Test
 #	   ls -d */ ---> Desktop/	Test/
-ITEMS=() #
+ITEMS=() # variable vacia
 for d in $LIST; do #para cada directorio de LIST
 ITEM="`ls $d | wc -l` -$d"
 #para cada directorio del parametro pasado, va a entrar en el mismo con ls $d
-#luego va a contar los "\n" --> casualmente devuelve la cantidad de directorios
-#entonces ITEM va a quedar asi "nroDirectorios -nombredirectorio/"
+#luego va a contar los "\n" ---> casualmente devuelve la cantidad de directorios
+#entonces ITEM va a quedar ---> "nroDirectorios -nombredirectorio/"
 ITEMS+=($ITEM) #append el item a ITEMS
 done
 IFS=$'\n' sorted=($(sort -rV -t '-' -k 1 <<<${ITEMS[*]}))
@@ -33,8 +39,8 @@ IFS=$'\n' sorted=($(sort -rV -t '-' -k 1 <<<${ITEMS[*]}))
 # -k --> key --> 1
 CANDIDATES="${sorted[*]:0:$2}" #selecciona los primeros X candidatos
 unset IFS #IFS vuelve a ser el espacio en blanco
-echo "........." #COMPLETAR
+echo "Subdirectorios" #COMPLETAR
 printf "%s\n" "$(cut -d '-' -f 2 <<<${CANDIDATES[*]})"
 # cut --> print selected parts of lines from each FILE to stdout
 # -d --> usar -d como delimitador
-# -f --> select onyl these fields 
+# -f --> select only these fields

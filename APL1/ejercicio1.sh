7932f2c57e3cdcf278fede17b6a0f84db05e45b2
Santiago <32301876+SantiCamm@users.noreply.github.com>
2021-09-06 18:40:16 -0300

    Update ejercicio1.sh

--- APL1/ejercicio1.sh
+++ APL1/ejercicio1.sh
@@ -7,7 +7,8 @@ echo "uso: $0 directorio 5" #COMPLETAR
 
 #funcB
 error(){
-echo "Error. $1 no es un directorio" #COMPLETAR}
+echo "Error. $1 no es un directorio" #COMPLETAR
+}
 
 #funcC
 validarParametros(){
@@ -27,11 +28,11 @@ ITEMS=() # variable vacia
 for d in $LIST; do #para cada directorio de LIST
 ITEM="`ls $d | wc -l` -$d"
 #para cada directorio del parametro pasado, va a entrar en el mismo con ls $d
-#luego va a contar los "\n" ---> casualmente devuelve la cantidad de directorios
-#entonces ITEM va a quedar ---> "nroDirectorios -nombredirectorio/"
+#luego va a contar los "\n" ---> casualmente devuelve la cantidad de archivos
+#entonces ITEM va a quedar ---> "nroArchivosEnSubdirectorio - nombreSubdirectorio"
 ITEMS+=($ITEM) #append el item a ITEMS
 done
-IFS=$'\n' sorted=($(sort -rV -t '-' -k 1 <<<${ITEMS[*]}))
+IFS=$'\n' sorted=($(sort -rV -t '-' -k 1 <<<"${ITEMS[*]}"))
 #SORT BY
 # -r --> reverse
 # -V --> version
@@ -39,8 +40,8 @@ IFS=$'\n' sorted=($(sort -rV -t '-' -k 1 <<<${ITEMS[*]}))
 # -k --> key --> 1
 CANDIDATES="${sorted[*]:0:$2}" #selecciona los primeros X candidatos
 unset IFS #IFS vuelve a ser el espacio en blanco
-echo "Subdirectorios" #COMPLETAR
-printf "%s\n" "$(cut -d '-' -f 2 <<<${CANDIDATES[*]})"
+echo "Nombre de archivo" #COMPLETAR
+printf "%s\n" "$(cut -d '-' -f 2 <<<"${CANDIDATES[*]}")"
 # cut --> print selected parts of lines from each FILE to stdout
 # -d --> usar -d como delimitador
 # -f --> select only these fields

09c31c8ce6591ede16b638685c36e8dd90a07ff0
SantiCamm <santi.cammarano@gmail.com>
2021-09-08 14:52:49 -0300

    more changes

--- APL1/ejercicio1.sh
+++ APL1/ejercicio1.sh
@@ -1,128 +1,101 @@
 #!/bin/bash
-#funcA --> nunca es llamada
+# Muestra un mensaje indicando el uso del script. Nunca es llamada
 help() {
-echo "Error, La sintaxis del script es la siguiente:"
-echo "uso: $0 <directorio_a_recorrer cantidad_elementos_a_mostrar>" #COMPLETAR
+	echo "Error, La sintaxis del script es la siguiente:"
+	echo "$0 <directorio cantDeSubdir>: $0 directorio 5"
+	exit 1;
 } 
 
-#funcB --> muestra un mensaje de error cuando el parametro $1 del script no es un directorio valido
-error(){
-echo "Error. $1 no es un directorio" #COMPLETAR
+# Muestra un mensaje de error cuando el parametro $1 del script no es un directorio valido
+error() {
+	echo "Error. $1 no es un directorio"
+	exit 1;
 }
 
-#funcC --> valida que el primer parametro enviado sea un directorio valido
-validarParametros(){
-if [[ ! -d $2 ]]; then
-error
-fi
+# Valida que el primer parametro enviado sea un directorio valido
+validarParametros() {
+	if [[ ! -d $2 ]]; then
+		error $2
+	fi
 }
-validarParametros $# $1 $2 $3 $4 $5 #validar parametros de entrada
 
-LIST=$(ls -d $1*/) #guarda una lista de subdirectorios a partir del directorio raiz pasado por parametro
-# LIST = `ls -d $1/` --> otra forma
-# ls -d lista todos las directory entries en el parametro que se le pase (no lista contenidos)
-# */ expande solo a los directorios
-# entonces agregandole -d solo te traes los directorios dentro del parametro pasado
-# EJEMPLO: ls -d * ---> Desktop		Test
-#	   ls -d */ ---> Desktop/	Test/
+# Validar parametros de entrada
+validarParametros $# $1 $2 $3 $4 $5
 
-ITEMS=() #variable vacia
+# Guarda una lista los subdirectorios/ presentes en el directorio raiz pasado por parametro
+LIST=$(ls -d $1*/)
 
-for d in $LIST; do #para cada directorio de LIST
-ITEM="`ls $d | wc -l` -$d" #ingresa en cada subdirectorio y cuenta la cantidad de elementos presentes alli junto con el nombre del subdirectorio
-#para cada directorio del parametro pasado, va a entrar en el mismo con ls $d
-<<<<<<< HEAD
-#luego va a contar los "\n" ---> casualmente devuelve la cantidad de directorios
-#entonces ITEM va a quedar ---> "nroDirectorios -nombredirectorio/"
+# Array vacio
+ITEMS=()
 
-ITEMS+=($ITEM) #append el item a ITEMS
+# Para cada subdirectorio del directorio pasado por parameto, se cuenta la cantidad de elementos presentes
+# Tambien se agrega el nombre del subdirectorio en cuestion
+for d in $LIST; do
+	# Formato de ITEM ---> "nroArchivosEnSubdirectorio-nombreSubdirectorio"
+	ITEM="`ls $d | wc -l` -$d"
+
+	# Agrega el item a ITEMS
+	ITEMS+=($ITEM) 
 done
 
-IFS=$'\n' sorted=($(sort -rV -t '-' -k 1 <<<"${ITEMS[*]}")) #guarda en una variable el listado ITEMS, ordenado por la cantidad de elementos (mayor a menor)
-=======
-#luego va a contar los "\n" ---> casualmente devuelve la cantidad de archivos
-#entonces ITEM va a quedar ---> "nroArchivosEnSubdirectorio - nombreSubdirectorio"
-ITEMS+=($ITEM) #append el item a ITEMS
-done
-IFS=$'\n' sorted=($(sort -rV -t '-' -k 1 <<<"${ITEMS[*]}"))
->>>>>>> bd132a7e3a7acd6bcf7a1904f1775194261daf9b
+# Cambia el Input Field Separator por '\n'
+# Guarda en un array, el listado anterior ordenado por la cantidad de elementos (de mayor a menor)
+IFS=$'\n' sorted=($(sort -rV -t '-' -k 1 <<<${ITEMS[*]}))
 #SORT BY
 # -r --> reverse
 # -V --> version
 # -t --> field separator = SEP --> '-'
 # -k --> key --> 1
 
-CANDIDATES="${sorted[*]:0:$2}" #selecciona el listado ordenado desde el 1er elemento hasta el numero de elementos pasado como parametro $2
-#ademas, se setea el separador de palabras a "\n"
+# Recorta el listado ordenado desde el 1er elemento hasta el numero de elementos pasado como parametro $2
+CANDIDATES="${sorted[*]:0:$2}"
 
-unset IFS #IFS vuelve a ser el espacio en blanco
-<<<<<<< HEAD
+# El IFS vuelve a ser el espacio en blanco
+unset IFS 
 
-echo "Subdirectorios" #COMPLETAR
-
-printf "%s\n" "$(cut -d '-' -f 2 <<<"${CANDIDATES[*]}")"
-# Muestra los subdirectorios junto con la cantidad de elementos presentes en ellos
-=======
-echo "Nombre de archivo" #COMPLETAR
-printf "%s\n" "$(cut -d '-' -f 2 <<<"${CANDIDATES[*]}")"
->>>>>>> bd132a7e3a7acd6bcf7a1904f1775194261daf9b
+# Muestra el listado generado de subdirectorios (mostrando solo el nombre de los mismos)
+echo "Nombre de subdirectorio"
+printf "%s\n" "$(cut -d '-' -f 2 <<<${CANDIDATES[*]})"
 # cut --> print selected parts of lines from each FILE to stdout
 # -d --> usar -d como delimitador
 # -f --> select only these fields
 
 
-<<<<<<< HEAD
-#1. El script cuenta la cantidad de subdirectorios existentes en todos los directorios presentes en el directorio enviado por parametro. Luego, crea una lista, la ordena con cierto orden e imprime los primeros X candidatos, siendo X el segundo parametro pasado al script. 
-#El script recibe 2 parametros: un directorio y un numero entero
-#4. Las funciones funcA, funcB y funcC se deberian llamar validar help, error y validarParametros respectivamente
-#5. Agregaria la validacion del segundo parametro para verificar que sea un numero entero. Ademas, le pasaria el parametro que contiene el directorio a funcB de la siguiente manera: funcB $2. Por otro lado, ante cualquier error en los parametros correspondiente al uso del script, deberia llamar a funcA para explicar dicho uso. Por ultimo, tanto funcA como funcB deberian finalizar con 'exit 1'.
-#6. La variable $# nos brinda la cantidad de parametros que le fueron pasadas al script.
+# 1. El objetivo del script es mostrar un listado de subdirectorios (de un directorio pasado por parametro) ordenados de mayor a menor por la cantidad de elementos que contienen. La cantidad maxima de subdirectorios a mostrar se pasa tambien por parametro.
+# El script recibe 2 parametros: un directorio y un numero que repesenta la cantidad de subdirectorios a mostrar.
+# 
+# 4. funcA: help()
+#    funcB: error()
+#    funcC: validarParametros()
+# 
+# 5. Agregaria/cambiaria:
+#	- Validar que la cantidad de parametros sea 2 y que el parametro $2 sea un numero valido (natural positivo)
+#	- Pasarle el parametro que contiene el directorio a funcB de la siguiente manera: funcB $2
+#	- Ante cualquier error en los parametros correspondiente al uso del script, deberia llamar a funcA para explicar dicho uso
+#	- Tanto funcA como funcB deberian finalizar con 'exit 1'
+#	- Las variables ${CANDIDATES[*]} e ${ITEMS[*]} no estaban correctamente encomilladas para ser interpretadas correctamente
+#
+# 6. La variable $# nos brinda la cantidad de parametros que le fueron pasadas al script.
 #	$1...${10} : parametros enviados
 #	$@ o $* : lista de parametros enviados
 #	$$ : PID del proceso
 #	$! : PID del ultimo proceso hijo ejecutado en 2do plano
 #	$? : valor de ejecucion del ultimo comando
-#7.
-#'Comillas simples': se utilizan para prevenir que bash interprete cualquier caracter especial dentro de las comillas. En otras palabras, se preserva el valor literal de cada caracter entre comillas. Ejemplo:
-#$ echo : 'Cualquier $ caracter que se pase aqui \ se mostrara 'literalmente' $'
-#Salida: Cualquier $ caracter que se pase aqui \ se mostrara 'literalmente' $
+# 
+# 7.
+# 'Comillas simples': se utilizan para prevenir que bash interprete cualquier caracter especial dentro de las comillas. En otras palabras, se preserva el valor literal de cada caracter entre comillas. Ejemplo:
+# $ echo : 'Cualquier $ caracter que se pase aqui \ se mostrara 'literalmente' $'
+# Salida: Cualquier $ caracter que se pase aqui \ se mostrara 'literalmente' $
 
-#"Comillas dobles": tambien preservan el valor literal de cada caracter entre comillas, pero permiten la interpretacion de caracteres especiales como $, `` o \. Ejemplo:
-#$ var=pepe
-#$ "Contenido \" de var es: $var"
-#Salida: Contenido " de var es: pepe
+# "Comillas dobles": tambien preservan el valor literal de cada caracter entre comillas, pero permiten la interpretacion de caracteres especiales como $, `` o \. Ejemplo:
+# $ var=pepe
+# $ "Contenido de var es: $var"
+# Salida: Contenido de var es: pepe
  
-#`Comillas invertidas`: forzan la ejecucion de los comandos dentro de las comillas. Luego de su ejecucion, la salida de los comandos reemplaza al contenido entre comillas, incluyendo a las mismas. 
-#Ejemplo: 
-#$ today=`date '+%A, %B %d, %Y'`
-#$ echo $today 
-#Salida: Lunes, Septiembre 06, 2021
-#8. Si el script es ejecutado sin ningun parametro, se llamara a funcB, la cual devolvera un mensaje de error.
-=======
-1. El script cuenta la cantidad de archivos existentes en todos los subdirectorios presentes en el directorio enviado por parametro. Luego, crea una lista, la ordena con cierto orden e imprime los primeros X candidatos, siendo X el segundo parametro pasado al script. 
-El script recibe 2 parametros: un directorio y un numero entero
-4. Las funciones funcA, funcB y funcC se deberian llamar validar help, error y validarParametros respectivamente
-5. Agregaria la validacion del segundo parametro para verificar que sea un numero entero. Ademas, le pasaria el parametro que contiene el directorio a funcB de la siguiente manera: funcB $2. Por otro lado, ante cualquier error en los parametros correspondiente al uso del script, deberia llamar a funcA para explicar dicho uso. Por ultimo, tanto funcA como funcB deberian finalizar con 'exit 1'.
-6. La variable $# nos brinda la cantidad de parametros que le fueron pasadas al script.
-	$1...${10} : parametros enviados
-	$@ o $* : lista de parametros enviados
-	$$ : PID del proceso
-	$! : PID del ultimo proceso hijo ejecutado en 2do plano
-	$? : valor de ejecucion del ultimo comando
-7.
-'Comillas simples': se utilizan para prevenir que bash interprete cualquier caracter especial dentro de las comillas. En otras palabras, se preserva el valor literal de cada caracter entre comillas. Ejemplo:
-$ echo : 'Cualquier $ caracter que se pase aqui \ se mostrara 'literalmente' $'
-Salida: Cualquier $ caracter que se pase aqui \ se mostrara 'literalmente' $
-
-"Comillas dobles": tambien preservan el valor literal de cada caracter entre comillas, pero permiten la interpretacion de caracteres especiales como $, `` o \. Ejemplo:
-$ var=pepe
-$ "Contenido \" de var es: $var"
-Salida: Contenido " de var es: pepe
- 
-`Comillas invertidas`: forzan la ejecucion de los comandos dentro de las comillas. Luego de su ejecucion, la salida de los comandos reemplaza al contenido entre comillas, incluyendo a las mismas. 
-Ejemplo: 
-$ today=`date '+%A, %B %d, %Y'`
-$ echo $today 
-Salida: Lunes, Septiembre 06, 2021
-8. Si el script es ejecutado sin ningun parametro, se llamara a funcB, la cual devolvera un mensaje de error.
->>>>>>> bd132a7e3a7acd6bcf7a1904f1775194261daf9b
+# `Comillas invertidas`: forzan la ejecucion de los comandos dentro de las comillas. Luego de su ejecucion, la salida de los comandos reemplaza al contenido entre comillas, incluyendo a las mismas. 
+# Ejemplo: 
+# $ today=`date '+%A, %B %d, %Y'`
+# $ echo $today 
+# Salida: Lunes, Septiembre 06, 2021
+#
+# 8. Si el script es ejecutado sin ningun parametro, se llamara a funcB, la cual devolvera un mensaje de error.

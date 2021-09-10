a315bdb47298793d60802312f3ff24ea17bc0be0
SantiCamm <santi.cammarano@gmail.com>
2021-09-10 12:03:09 -0300

    5 done

--- APL1/ejercicio5.sh
+++ APL1/ejercicio5.sh
@@ -5,7 +5,7 @@ ayuda="
 Ayuda correspondiente al script ejercicio5.sh
 
 #OBJETIVO PRINCIPAL#
-Este script permite generar archivos .zip en base a logs viejos almacenados
+Este script permite generar archivos .zip en base a logs viejos almacenados en rutas provistas en un archivo de configuracion
 
 #USO#
 Uso: ./ejercicio5.sh archivoDeConfiguracion.txt
@@ -15,7 +15,9 @@ Ejemplo: ./ejercicio5.sh /Configuracion/configuracion.txt
 	archivoDeConfiguracion.txt: archivo .txt con los directorios correspondientes
 
 #ACLARACIONES#
-
+- El archivo de configuracion debe tener la siguiente estructura:
+	- En la primera linea la carpeta de destino de los archivos comprimidos
+	- A partir de la segunda linea, las rutas donde se encuentran las carpetas de logs a analizar
 "
 
 uso="Uso del script: ./ejercicio5.sh archivoDeConfiguracion.txt"
@@ -33,25 +35,15 @@ help() {
 	exit 1;
 }
 
-
 # Funcion que valida si el parametro pasado es un directorio (y su existencia como path)
 validarDirectorio() {
-	#realpath -e "$1" > /dev/null 2>&1
-	#if [[ $? == 1 ]]; then
-	#	help "La ruta provista en el archivo de configuracion no existe"
-	#fi
-
-	#path=`realpath -e "$1"`
-
-#	path=`dirname "$path"`
-	echo "Path a analizar: $1"
 	if [[ ! -d "$1" ]]
 	then
 		help "La ruta provista en el archivo de configuracion no es directorio valido"
 	fi
 }
 
-#
+# Funcion que obtiene el path absoluto del parametro pasado
 obtenerPathAbsoluto() {
 	realpath -e "$1" > /dev/null 2>&1
 	if [[ $? == 1 ]]; then
@@ -65,7 +57,7 @@ obtenerPathAbsoluto() {
 verificarArchivoDeConfiguracion() {
 	path=`realpath "$1"`
 	#path=`dirname "$path"`
-	echo "Archivo de configuracion a analizar: $path"
+	#echo "Archivo de configuracion a analizar: $path"
 
 	if [[ -f $1 ]]; then
 		if [[ $1 != *.txt ]]; then
@@ -110,49 +102,25 @@ listaDirectoriosBase=(`awk 'NR>1 { print $0 }' "$1"`)
 
 for i in "${!listaDirectoriosBase[@]}"; do
 	listaDirectoriosBase[$i]=`obtenerPathAbsoluto "${listaDirectoriosBase[$i]}"`
+	#echo "${listaDirectoriosBase[$i]}"
 	validarDirectorio "${listaDirectoriosBase[$i]}"
 done
 
 # Obtengo el nombre del directorio padre de cada una de las rutas de directorios de logs
-listArchivo=(`awk 'BEGIN { FS = "/" } ; NR>1 {print $(NF-1)}' "$1"`)
+nombresDirPadre=(`awk 'BEGIN { FS = "/" } ; NR>1 {print $(NF-1)}' "$1"`)
 
 # Creo un array conteniendo los nombres de los archivos .zip a crear
-ITEMSZIP=()
-for i in "${!listArchivo[@]}"; do
+nombreZips=()
+for i in "${!nombresDirPadre[@]}"; do
 	fec=$(printf '%(%Y%m%d_%H%M%S)T')
-	ITEM=logs_"${listArchivo[i]}""_$fec.zip"
-	ITEMSZIP+=("$ITEM")
+	ITEM=logs_"${nombresDirPadre[i]}""_$fec.zip"
+	nombreZips+=("$ITEM")
 done
 
+# Entro en cada directorio para buscar los logs viejos, los zipeo y guardo en la carpeta de destino. Finalmente, los elimino
 for i in "${!listaDirectoriosBase[@]}"; do
 	cd "${listaDirectoriosBase[i]}"
-	#find . -mtime +1 | zip "${ITEMSZIP[i]}" *txt
-	find . -mtime +1 -iname '*.txt' -o -iname '*.log' -o -iname '*.info' | zip "$directorioDestino/${ITEMSZIP[i]}" -@ 
+	#usar esta #find . -mtime +1 -iname '*.txt' -o -iname '*.log' -o -iname '*.info' | zip -rm "$directorioDestino/${nombreZips[i]}" -@ 
+	find . -mtime +1 -iname '*.txt' -o -iname '*.log' -o -iname '*.info' | zip "$directorioDestino/${nombreZips[i]}" -@ 
 done
-exit 1
-DirectorioZip="${ITEMSDIR[0]}/${ITEMSZIP[1]}"
-echo "Archivos del cual debo comprimir " $(realpath ${ITEMSDIR[1]})" ;Archivo zip $DirectorioZip"
-zip "$DirectorioZip" "$(realpath ${ITEMSDIR[1]})"
-DirectorioZip="${ITEMSDIR[0]}/${ITEMSZIP[2]}"
-echo "Archivos del cual debo comprimir " $(realpath ${ITEMSDIR[2]})" ;Archivo zip $DirectorioZip"
-zip "$DirectorioZip" "$(realpath ${ITEMSDIR[2]})"
-DirectorioZip="${ITEMSDIR[0]}/${ITEMSZIP[3]}"
-echo "Archivos del cual debo comprimir " $(realpath ${ITEMSDIR[3]})" ;Archivo zip $DirectorioZip"
-zip "$DirectorioZip" "$(realpath ${ITEMSDIR[3]})"
-#Luego de Zipear debo eliminar lo zipeado por defecto va a elimianr lo que este en ./tmp/servicios/ubicaciones/
-rm -r ${ITEMSDIR[3]}
-exit
-#############Comprobando funciones sobre los nombres de los directorios
-#director="C:/Users/fernando/Desktop/SO 2C 2021/Configuracion/Configuracion.txt"
-#dirname "C:/Users/fernando/Desktop/SO 2C 2021/Configuracion/Configuracion.txt"
-#basename "C:\Users\fernando\Desktop\SO 2C 2021\Configuracion\Configuracion.txt"
-#dir2=${director%/*}
-#echo "${director##*/}"
-#echo "${director##*.}"
-#echo "------------------------"
-#echo "$dir2"
-#echo "${dir2##*/}"
-#echo "<--Nombre: ${d%.*}" "-->"
-#echo "<--Extension: ${d##*.}" "-->"
-#echo "${director%/*}"
 # ---------------------------------- FIN PROGRAMA ----------------------------------
